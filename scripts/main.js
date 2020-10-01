0f86f824c794dea4f17d030246d4b9da299fd4bb
DeltaNedas <deltanedas@gmail.com>
2020-10-01 19:09:18 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+var buffer, shader, rabbit;
+
+function beginDraw() {
+	buffer.resize(Core.graphics.width, Core.graphics.height);
+	buffer.begin(Color.clear);
+}
+
+function endDraw() {
+	buffer.end();
+
+	shader.bind();
+	shader.setUniformf("u_time", Time.globalTime() / 100.0);
+
+	Draw.blit(buffer, shader);
+}
+
+Events.on(ClientLoadEvent, e => {
+	buffer = new FrameBuffer(Core.graphics.width, Core.graphics.height);
+	rabbit = Core.atlas.find("rabbit");
+	shader = new Shader(readString("shaders/screenspace.vert"), readString("shaders/hell.frag"));
+});
+
+Events.run(Trigger.preDraw, beginDraw);
+
+Events.run(Trigger.uiDrawBegin, () => {
+	if (Vars.state.isMenu()) {
+		beginDraw();
+	}
+});
+
+Events.run(Trigger.uiDrawEnd, endDraw);
---
+++ shaders/hell.frag
@@ -0,0 +1,16 @@
+uniform sampler2D u_texture;
+uniform float u_time;
+
+varying vec2 v_texCoords;
+
+#define destColor vec4(2.0, 0.2, 0.3, 0.5)
+
+void main(){
+	vec2 uv = v_texCoords * 8.0;
+	uv.y += cos(u_time * 0.85 + uv.x);
+	uv.x += sin(u_time + uv.y);
+	uv += sin(u_time + uv * 3.1415) + sin(u_time + uv.x * uv.y * 0.42);
+
+	uv = mod(uv, 1.0);
+	gl_FragColor = texture2D(u_texture, uv);
+}
